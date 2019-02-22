4522306d4360d1bc2b107fdd9ffb8b7dd50757a7
Dhipi <Dhipi@DESKTOP-480DBP8>
2019-02-22 18:15:31 +0530

    Add a new class to HelloWorld_Pack

---
+++ Git_HelloWorld/src/HelloWorld_PAck/NewClass.java
@@ -0,0 +1,10 @@
+package HelloWorld_PAck;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a new class");
+	}
+
+}
